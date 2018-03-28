a248230479d4f1cf8e2c01ebc08ba8affd651c88
Sagnik <saaggy18@gmail.com>
2018-03-28 12:25:17 +0530

    changes config of pass

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     service: 'Gmail',
     auth: {
       user: 'rabart18@gmail.com',
-      pass: 'xxxxx'
+      pass: 'iamlegend2112'
     }
   },
   dbConnString: 'mongodb://admin:123456@ds125479.mlab.com:25479/node-collaborator',
